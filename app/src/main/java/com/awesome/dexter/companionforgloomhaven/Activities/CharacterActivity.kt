15f4e8a3330607470799c7d8ca98f274bf0f61bf
Dan Dexter <dexter.jdexter@gmail.com>
2017-05-22 13:54:32 -0500

    Passed off character to character activity.

--- app/src/main/java/com/awesome/dexter/companionforgloomhaven/Activities/CharacterActivity.kt
+++ app/src/main/java/com/awesome/dexter/companionforgloomhaven/Activities/CharacterActivity.kt
@@ -2,16 +2,29 @@ package com.awesome.dexter.companionforgloomhaven.Activities
 
 import android.app.Activity
 import android.os.Bundle
+import com.awesome.dexter.companionforgloomhaven.Characters.*
+import com.awesome.dexter.companionforgloomhaven.Characters.Character_Table.id
 import com.awesome.dexter.companionforgloomhaven.R.layout.activity_character
+import com.raizlabs.android.dbflow.config.FlowManager
+import com.raizlabs.android.dbflow.kotlinextensions.*
+import com.raizlabs.android.dbflow.sql.language.SQLite
 
 /**
  * Created by Dan on 5/20/2017.
  */
 class CharacterActivity : Activity(){
+    var character: Character? = Character()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_character)
 
-        val character = intent.extras.getInt(CHARACTER_KEY)
+        FlowManager.init(this)
+
+        val characterId = intent.extras.getInt(CHARACTER_KEY)
+        SQLite.select().from(Character::class)
+        character = (select from Character::class where (id eq characterId)).result
+        title = character?.name ?: "Error"
+
     }
 }
