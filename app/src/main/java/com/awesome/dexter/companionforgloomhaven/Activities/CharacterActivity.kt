ff466a222afef7b45197df681d4250513e668729
Dan <dexter.jdexter@gmail.com>
2017-07-22 13:26:23 -0500

    Did some playing with before with the toolbar.  Updated project with some recent android tool updates.

--- app/src/main/java/com/awesome/dexter/companionforgloomhaven/Activities/CharacterActivity.kt
+++ app/src/main/java/com/awesome/dexter/companionforgloomhaven/Activities/CharacterActivity.kt
@@ -2,6 +2,7 @@ package com.awesome.dexter.companionforgloomhaven.Activities
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.awesome.dexter.companionforgloomhaven.Characters.*
 import com.awesome.dexter.companionforgloomhaven.Characters.Character_Table.id
 import com.awesome.dexter.companionforgloomhaven.R.layout.activity_character
@@ -13,12 +14,13 @@ import kotlinx.android.synthetic.main.activity_character.*
 /**
  * Created by Dan on 5/20/2017.
  */
-class CharacterActivity : Activity(){
+class CharacterActivity : AppCompatActivity(){
     var character = Character()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_character)
+        setSupportActionBar(CharacterToolbar)
 
         FlowManager.init(this)
 
@@ -27,5 +29,7 @@ class CharacterActivity : Activity(){
         character = (select from Character::class where (id eq characterId)).result ?: Character()
         title = character.name
         CharacterRace.text = character.getCharacterRace()
+        CharacterLevel.text = character.level.toString()
+        CharacterExperience.text = character.experience.toString()
     }
 }
