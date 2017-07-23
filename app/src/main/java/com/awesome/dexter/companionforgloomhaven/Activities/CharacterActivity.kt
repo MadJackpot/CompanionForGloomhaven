7f50911b6a1a4ecbfa6916bcd14cf02cb9e96b9c
Dan <dexter.jdexter@gmail.com>
2017-07-22 21:21:50 -0500

    Added realm capability to character activity.

--- app/src/main/java/com/awesome/dexter/companionforgloomhaven/Activities/CharacterActivity.kt
+++ app/src/main/java/com/awesome/dexter/companionforgloomhaven/Activities/CharacterActivity.kt
@@ -4,24 +4,25 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.awesome.dexter.companionforgloomhaven.Characters.Character
 import com.awesome.dexter.companionforgloomhaven.R.layout.activity_character
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_character.*
+import kotlin.properties.Delegates
 
 /**
  * Created by Dan on 5/20/2017.
  */
 class CharacterActivity : AppCompatActivity(){
     var character = Character()
+    private var realm: Realm by Delegates.notNull()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_character)
         setSupportActionBar(CharacterToolbar)
-
-        //FlowManager.init(this)
+        realm = Realm.getDefaultInstance()
 
         val characterId = intent.extras.getInt(CHARACTER_KEY)
-        //SQLite.select().from(Character::class)
-        character = Character()//(select from Character::class where (id eq characterId)).result ?: Character()
+        character = realm.where(Character::class.java).findAll().filter{it.id == characterId}.firstOrNull() ?: Character()
         title = character.name
         CharacterRace.text = character.getCharacterRace()
         CharacterLevel.text = character.level.toString()
--- app/src/main/java/com/awesome/dexter/companionforgloomhaven/Database/CharacterQueries.kt
+++ app/src/main/java/com/awesome/dexter/companionforgloomhaven/Database/CharacterQueries.kt
@@ -13,15 +13,6 @@ object CharacterQueries {
         return realm.where(Character::class.java).findAll().toMutableList()
     }
 
-    fun SaveCharacters(characters: List<Character>){
-        var realm = Realm.getDefaultInstance()
-        realm.executeTransaction{
-            for (character in characters) {
-                realm.createObject(Character::class.java)
-            }
-        }
-    }
-
     fun GetNextID(): Int {
         var realm = Realm.getDefaultInstance()
         return realm.where(Character::class.java).findAll().lastIndex + 1
